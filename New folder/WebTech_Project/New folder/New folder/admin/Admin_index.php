37dacaa9f5d28bd06bd614b219e83029a98e968c
HasanSarower <sarower.hasan@outlook.com>
2017-12-28 02:44:07 +0600

    Add files via upload

---
+++ New folder/WebTech_Project/New folder/New folder/admin/Admin_index.php
@@ -0,0 +1,47 @@
+
+
+<table width="70%" cellspacing="0" cellpadding="10" border="1" align="center">
+    <tr>
+        <td colspan="2" valign="middle" height="30">
+            <table width="100%">
+                <tr>
+                    <td>
+                        <a href="../dashboard.html" target="contentFrame">
+                            <img alt="SHEBA" width="130" height="70" src="Capture.png">
+                        </a>
+                    </td>
+                    <td align="right">
+                        Logged in as <a href="Admin_profile.html" target="contentFrame">Admin</a>&nbsp;|
+                        <a href="settings.html" target="contentFrame">Settings</a>&nbsp;|
+                        <a href="../web_index.html">Logout</a>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+    <tr>
+        <td width="200" valign="top">
+        	<font size="4">
+        	<ul>
+        	<li><a href="../dashboard.html" target="contentFrame">Dashboard</a></li>
+        	<li><a href="Notifications.html" target="contentFrame"><label>Notifications</label></a></li>
+            <li><a href="User_Management.html" target="contentFrame"><label>User Management</label></a></li>
+            <li><a href="User_reports.html" target="contentFrame"><label>User Report</label></a></li>
+            <li><a href="registration.html" target="contentFrame"><label>Add New Admin</label></a></li>
+            <li><a href="../app/add_delete_meds.php" target="contentFrame"><label>Medicine Management</label></a></li>
+            <li><a href="../app/add_delete_meds.php" target="contentFrame"><label>Add Deal</label></a></li>
+            <li><a href="../app/add_delete_meds.php" target="contentFrame"><label>Add Other Products</label></a></li>
+
+        	</ul>
+        </font>
+        </td>
+        <td valign="top">
+            <iframe name="contentFrame" frameborder="0" width="100%" height="530" src="../dashboard.html"></iframe>
+        </td>
+    </tr>
+    <tr>
+        <td colspan="2" align="center">
+            Copyright &copy; 2017
+        </td>
+    </tr>
+</table>
