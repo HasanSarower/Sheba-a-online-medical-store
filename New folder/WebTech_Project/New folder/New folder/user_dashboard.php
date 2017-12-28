1d096e5f0a3f88a2f644b2677bdf871f541b48d3
faadshahrukh <faadshahrukh@gmail.com>
2017-12-28 17:00:30 +0600

    updated

--- New folder/WebTech_Project/New folder/New folder/user_dashboard.php
+++
@@ -1,7 +0,0 @@
-<table bgcolor="white" align="center" width="100%" height="100%" border="0" cellspacing="7">
-	<tr height="100">
-		<td >
-			<h1 align="center">Welcome to our site</h1>
-		</td>
-	</tr>
-</table>
